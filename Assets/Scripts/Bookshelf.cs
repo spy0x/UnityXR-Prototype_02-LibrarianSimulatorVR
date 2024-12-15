bc44c122e8ad2fc982070dc57550a5a9de1b8de2
Francisco V. Diaz <fevarga1@uc.cl>
2024-12-15 03:18:27 -0300

    added book placement and grabbing. books scale bigger

--- Assets/Scripts/Bookshelf.cs
+++ Assets/Scripts/Bookshelf.cs
@@ -1,30 +1,32 @@
 using System;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.XR.Interaction.Toolkit.AffordanceSystem.State;
+using UnityEngine.XR.Interaction.Toolkit.Interactables;
+using UnityEngine.XR.Interaction.Toolkit.Interactors;
 using UnityEngine.XR.Interaction.Toolkit.Locomotion.Climbing;
 
 public class Bookshelf : MonoBehaviour
 {
-    [SerializeField] ClimbInteractable climbInteractable;
-    [SerializeField] Transform teleportAnchor;
+    [SerializeField] XRSocketInteractor socketInteractor;
 
-    // private void OnEnable()
-    // {
-    //     climbInteractable.selectEntered.AddListener(OnSelectEntered);
-    // }
-    //
-    // private void OnDisable()
-    // {
-    //     climbInteractable.selectEntered.RemoveListener(OnSelectEntered);
-    // }
-
-    private void OnSelectEntered(SelectEnterEventArgs arg0)
+    private void OnEnable()
     {
-        Vector3 playerRelative = arg0.interactorObject.transform.GetComponentInParent<CharacterController>().transform.InverseTransformPoint(teleportAnchor.position);
-        Vector3 teleportAnchorPos = teleportAnchor.localPosition;
-        teleportAnchorPos.z -= playerRelative.z * 2;
-        teleportAnchor.localPosition = teleportAnchorPos;
-        Debug.Log(playerRelative);
-        Debug.Log(teleportAnchorPos);
+        socketInteractor.selectExited.AddListener(OnSelectExited);
     }
+    
+    private void OnDisable()
+    {
+        socketInteractor.selectExited.RemoveListener(OnSelectExited);
+    }
+
+    private void OnSelectExited(SelectExitEventArgs arg0)
+    {
+        XRGrabInteractable grabInteractable = arg0.interactableObject as XRGrabInteractable;
+        if (!grabInteractable) return;
+        grabInteractable.enabled = false;
+        XRInteractableAffordanceStateProvider affordanceStateProvider = grabInteractable.GetComponent<XRInteractableAffordanceStateProvider>();
+        if (affordanceStateProvider) affordanceStateProvider.enabled = false;
+    }
+    
 }
