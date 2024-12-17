107d7a7ff1126ae92c0d09f291aec149814216f8
Francisco V. Diaz <fevarga1@uc.cl>
2024-12-17 17:48:33 -0300

    made running behaviour working. need to make follow patrol now.

--- Assets/Scripts/AI/PeopleBehaviour.cs
+++ Assets/Scripts/AI/PeopleBehaviour.cs
@@ -1,6 +1,9 @@
 using System;
+using System.Numerics;
 using UnityEngine;
 using UnityEngine.AI;
+using Vector2 = UnityEngine.Vector2;
+using Vector3 = UnityEngine.Vector3;
 
 
 public enum PeopleState
@@ -16,21 +19,31 @@ public enum PeopleState
 public class PeopleBehaviour : MonoBehaviour
 {
     private static readonly int Speed = Animator.StringToHash("Speed");
-    [Header("AI Settings")]
+
+    [Header("AI Settings")] [SerializeField]
+    private Transform head;
+
     [SerializeField] private PeopleState initialState;
     [SerializeField] private float angleView = 45f;
+    [SerializeField] private float runningAngleView = 360f;
     [SerializeField] private float distanceView = 5f;
-    [SerializeField] [Range(0,1)] private float chanceToGoIdle = 0.3f;
+    [SerializeField] private float runningDistanceView = 15f;
+    [SerializeField] [Range(0, 1)] private float chanceToGoIdle = 0.3f;
     [SerializeField] Vector2 randomIdleTime = new Vector2(2, 5);
-    [Header("Locomotion Settings")] 
-    [SerializeField] private float walkingSpeed = 1f;
+    [SerializeField] private float playerPresenceDistance = 3f;
+
+    [Header("Locomotion Settings")] [SerializeField]
+    private float walkingSpeed = 1f;
+
     [SerializeField] private float runningSpeed = 3f;
     [SerializeField] private PatrolPoints patrolPoints;
     private PeopleState currentState;
+    private float currentDistanceView;
+    private float currentAngleView;
     private Vector3 playerLastPosition;
     private Transform player; // Reference to the player
     private bool isWaiting = false;
-    
+
     public PeopleState CurrentState
     {
         get => currentState;
@@ -40,9 +53,12 @@ public class PeopleBehaviour : MonoBehaviour
     private NavMeshAgent navMeshAgent;
     private float timer;
     private float idleTime;
+    private static bool hasTarget;
 
     private void Start()
     {
+        currentDistanceView = distanceView;
+        currentAngleView = angleView;
         player = GameObject.FindGameObjectWithTag("MainCamera").transform;
         animator = GetComponent<Animator>();
         navMeshAgent = GetComponent<NavMeshAgent>();
@@ -54,28 +70,27 @@ public class PeopleBehaviour : MonoBehaviour
         switch (currentState)
         {
             case PeopleState.Idle:
+                navMeshAgent.speed = 0;
                 animator.SetFloat(Speed, 0);
-                EnableMovement(false);
                 Idleling();
                 break;
             case PeopleState.Walking:
-                animator.SetFloat(Speed, walkingSpeed);
-                EnableMovement(true);
                 navMeshAgent.speed = walkingSpeed;
+                animator.SetFloat(Speed, walkingSpeed);
                 Walking();
                 break;
             case PeopleState.Running:
-                animator.SetFloat(Speed, runningSpeed);
-                EnableMovement(true);
                 navMeshAgent.speed = runningSpeed;
+                animator.SetFloat(Speed, runningSpeed);
+                Running();
                 break;
             case PeopleState.Reading:
                 animator.SetFloat(Speed, 0);
                 EnableMovement(false);
                 break;
             case PeopleState.Talking:
+                navMeshAgent.speed = 0;
                 animator.SetFloat(Speed, 0);
-                EnableMovement(false);
                 break;
             case PeopleState.Searching:
                 animator.SetFloat(Speed, 0);
@@ -88,11 +103,9 @@ public class PeopleBehaviour : MonoBehaviour
 
     private void Idleling()
     {
-        if (IsPlayerInSight())
+        if (IsPlayerInSight() && !hasTarget)
         {
-            currentState = PeopleState.Running;
-            playerLastPosition = player.position;
-            navMeshAgent.SetDestination(playerLastPosition);
+            ChasePlayer();
         }
         else if (isWaiting)
         {
@@ -104,49 +117,113 @@ public class PeopleBehaviour : MonoBehaviour
             }
         }
     }
-    
+
     private void Walking()
     {
-        if (patrolPoints)
+        if (IsPlayerInSight() && !hasTarget)
         {
-            if (Vector3.Distance(transform.position, patrolPoints.GetCurrentPatrolPoint().position) <= navMeshAgent.stoppingDistance)
+            ChasePlayer();
+        }
+        else if (patrolPoints)
+        {
+            if (Vector3.Distance(transform.position, patrolPoints.GetCurrentPatrolPoint().position) <=
+                navMeshAgent.stoppingDistance)
             {
                 patrolPoints.GetNextPatrolPoint();
                 if (UnityEngine.Random.value <= chanceToGoIdle)
                 {
-                    isWaiting = true;
-                    timer = 0;
-                    idleTime = UnityEngine.Random.Range(randomIdleTime.x, randomIdleTime.y);
-                    currentState = PeopleState.Idle;
+                    WaitRandomTime();
                     return;
                 }
             }
+
             navMeshAgent.SetDestination(patrolPoints.GetCurrentPatrolPoint().position);
         }
-        if (IsPlayerInSight())
+    }
+
+
+
+    private void Running()
+    {
+        if (Vector3.Distance(transform.position, player.position) <= playerPresenceDistance || IsPlayerInSight())
         {
-            currentState = PeopleState.Running;
-            playerLastPosition = player.position;
+            NavMeshPath path = new NavMeshPath();
+            if (navMeshAgent.CalculatePath(player.position, path))
+            {
+                if (path.status == NavMeshPathStatus.PathComplete)
+                {
+                    playerLastPosition = player.position;
+                    navMeshAgent.SetDestination(player.position);
+                }
+                else if (playerLastPosition != Vector3.zero)
+                {
+                    navMeshAgent.SetDestination(playerLastPosition);
+                }
+            }
+            else if (playerLastPosition != Vector3.zero)
+            {
+                navMeshAgent.SetDestination(playerLastPosition);
+            }
+            else if (patrolPoints)
+            {
+                WaitRandomTime();
+            }
+        }
+        else if (playerLastPosition != Vector3.zero)
+        {
             navMeshAgent.SetDestination(playerLastPosition);
         }
+        else if (patrolPoints)
+        {
+            WaitRandomTime();
+        }
+        if (Vector3.Distance(transform.position, navMeshAgent.destination) <= navMeshAgent.stoppingDistance)
+        {
+            WaitRandomTime();
+        }
+        
     }
-    
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (currentState == PeopleState.Running && other.transform == player)
+        {
+            Debug.Log("Player entered the trigger");
+            isWaiting = false;
+            SetState(PeopleState.Talking);
+        }
+    }
+
     private bool IsPlayerInSight()
     {
         Vector3 directionToPlayer = player.position - transform.position;
         float angleToPlayer = Vector3.Angle(transform.forward, directionToPlayer);
 
-        if (directionToPlayer.magnitude <= distanceView && angleToPlayer <= angleView / 2)
+        if (directionToPlayer.magnitude <= currentDistanceView && angleToPlayer <= angleView / 2)
         {
-            if (Physics.Linecast(transform.position, player.position, out RaycastHit hit))
+            if (Physics.Linecast(head.position, player.position, out RaycastHit hit))
             {
-                if (hit.transform.CompareTag("Player"))
-                {
-                    return true;
-                }
+                return hit.transform == player;
             }
         }
+
         return false;
+    }
+
+    private void WaitRandomTime()
+    {
+        currentDistanceView = distanceView;
+        currentAngleView = angleView;
+        isWaiting = true;
+        timer = 0;
+        idleTime = UnityEngine.Random.Range(randomIdleTime.x, randomIdleTime.y);
+        currentState = PeopleState.Idle;
+    }
+    private void ChasePlayer()
+    {
+        currentDistanceView = runningDistanceView;
+        currentAngleView = runningAngleView;
+        currentState = PeopleState.Running;
     }
 
     public void EnableMovement(bool state)
